cbd3f5e8ed94ef422e45b25b67c21f70b0641fc4
silviamontejo507 <silviamontejo507@gmail.com>
2025-03-18 06:50:45 +0000

    Add 13.php

---
+++ src/13.php
@@ -0,0 +1,10 @@
+<?php
+    // Random number between 1 and 10
+    $randomNumber = mt_rand(1, 10);
+
+    // Calculate the square root of the random number
+    $squareRoot = sqrt($randomNumber);
+
+    // Print the result
+    echo "The square root of $randomNumber is $squareRoot";
+?>
