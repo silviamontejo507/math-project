2832affb92a4f47a4e044a942482de84fa495091
silviamontejo507 <silviamontejo507@gmail.com>
2025-04-06 13:26:56 +0000

    Add 31.php

---
+++ src/31.php
@@ -0,0 +1,12 @@
+<?php
+// Define some variables and constants
+$pi = 3.14159;
+$a = 2;
+$b = 3;
+
+// Calculate the area of a circle
+$circle_area = pi * $a * $b / 2;
+
+// Print the result to the console
+echo "The area of the circle with radius {$a} and diameter {$b} is: " . $circle_area;
+?>
