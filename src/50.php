d022637f67c537c0437742523f907a982f652c82
silviamontejo507 <silviamontejo507@gmail.com>
2025-04-11 11:46:37 +0000

    Add 50.php

---
+++ src/50.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants
+$var1 = "value1";
+$var2 = 42;
+$var3 = "Hello World";
+
+// Perform some calculations
+$result = $var1 + $var2 * $var3;
+
+// Output the result
+echo "The sum is: " . $result;
+?>
