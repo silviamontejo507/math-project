2724e46ee4c4701938195bf9082db32e10a524ca
silviamontejo507 <silviamontejo507@gmail.com>
2025-05-19 09:19:06 +0000

    Add 57.php

---
+++ src/57.php
@@ -0,0 +1,22 @@
+<?php
+// Example PHP code to demonstrate some basic operations and variables
+
+$first_number = 5;
+$second_number = 3;
+
+// Adding two numbers
+$total = $first_number + $second_number;
+echo "The sum of $first_number and $second_number is: " . $total . "\n";
+
+// Subtracting one number from another
+$difference = $first_number - $second_number;
+echo "The difference between $first_number and $second_number is: " . $difference . "\n";
+
+// Multiplying two numbers
+$product = $first_number * $second_number;
+echo "The product of $first_number and $second_number is: " . $product . "\n";
+
+// Dividing one number by another
+$quotient = $first_number / $second_number;
+echo "The quotient of $first_number divided by $second_number is: " . $quotient . "\n";
+?>
