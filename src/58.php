bb882ee03a44379fe70bb5ee7eaa1c0085bd09af
silviamontejo507 <silviamontejo507@gmail.com>
2025-05-20 04:57:24 +0000

    Add 58.php

---
+++ src/58.php
@@ -0,0 +1,9 @@
+<?php
+// Sample PHP code to demonstrate basic arithmetic operations
+$firstNumber = 10;
+$secondNumber = 5;
+
+$result = $firstNumber + $secondNumber; // Sum of first and second number
+
+echo "The sum of $firstNumber and $secondNumber is: $result";
+?>
