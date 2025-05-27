0b31581a979f94f404ca309f2c1fea42ea9b3a2c
silviamontejo507 <silviamontejo507@gmail.com>
2025-05-27 15:11:15 +0000

    Add 63.php

---
+++ src/63.php
@@ -0,0 +1,22 @@
+<?php
+// Example PHP code for a simple calculator
+function add($a, $b) {
+    return $a + $b;
+}
+
+function subtract($a, $b) {
+    return $a - $b;
+}
+
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+function divide($a, $b) {
+    if ($b != 0) {
+        return $a / $b;
+    } else {
+        throw new Exception("Divison by zero is not allowed!");
+    }
+}
+?>
